f1ebd073dc1e52c60231d476d8c5dbe4c4681719
Aastha Goyal <aastha2150@gmail.com>
2022-02-16 21:58:30 +1300

    Commiting the final result

--- PrimeNumbers/Program.cs
+++ PrimeNumbers/Program.cs
@@ -4,105 +4,100 @@ namespace PrimeNumbers
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Application started");
+            Console.WriteLine("Application started \n");
+
+            // Get the list of primes between 0-1000
+            List<long> primes = getPrimes(1000);
+
+            //Calculate the product of all 4 possible primes 
+         
+            for (int i = 0; i < primes.Count; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    for (int k = 0; k <= j; k++)
+                    {
+                        for (int l = 0; l <= k; l++)
+                        {
+                            long product = primes[i] * primes[j] * primes[k] * primes[l];
+                            
+                            if(checkSequence(product))
+                            {
+                                Console.Write("The four prime numbers are " + primes[i] + ", " + primes[j] + ", " + primes[k] + " and " + primes[l] + "\n");
+                                Console.Write("The product is " + product + "\n");
+                            }
+
+                        }
+                    }
+                }
+
+            }
+            Console.WriteLine("Application completed \n");
+        }
+
+        //Function to get all the prime numbers between 0-limit(1000)
+        static List<long> getPrimes(int limit)
+        {
             List<long> primes = new List<long>();
-            
+
             int ctr = 0;
 
-            for (int i=2;i<1000;i++)
+            for (int i = 2; i < limit; i++)
             {
                 ctr = 0;
-        
-                for(int j=2;j<i;j++)
+
+                for (int j = 2; j < i; j++)
                 {
                     if (i % j == 0)
                     {
                         ctr = 1;
                         break;
-                        
                     }
-
                 }
-
-                if(ctr==0)
+                if (ctr == 0)
                 {
                     primes.Add(i);
                 }
-                
             }
 
-            
-            for(int m=0;m<primes.Count;m++)
+            return primes;
+        }
+
+        //Function to check the conditions on the product 
+        static bool checkSequence(long product)
+        {
+            double length = Math.Floor(Math.Log10(product) + 1);
+            if(length != 12)
             {
-                for (int n = 1; n < primes.Count - 2; n++)
-                {
-                    
-                    long product = (long)primes[m] * (long)primes[n] * (long)primes[n + 1] * (long)primes[n + 2];
-                    
-                    double length = Math.Floor(Math.Log10(product) + 1);
-                    
-                    //Console.Write("the product is" + product + "and length" + length + "\n");
-                    if (length == 12)
-                    {
-                       
-                        String productStr = product.ToString();
-                        char[] digitsChar = new char[12];
-                        for(int i=0;i<12;i++)
-                        {
-                            digitsChar[i] = Convert.ToChar(productStr[i]);
-                        }
-                      
-                        int[] digitsInt = new int[12];
-                        for (int i = 0; i < 12; i++)
-                        {
-                            digitsInt[i] = Convert.ToInt32(new String(digitsChar[i], 1));
-                        }
-                        // Array.ForEach(digitsInt, Console.WriteLine);
-                        //Console.Write(primes[m] + "and" + primes[n] + "and" + primes[n + 1] + "and" + primes[n + 2] + "\n");
-                        //Console.Write("The product has 12 digits " + product + "and length" + length + "\n");
-                        bool result = false;
-
-                        //Array.ForEach(digitsInt, Console.WriteLine);
-                        //Console.Write("The product has 12 digits " + product + "\n");
-
-                        for (int k=0;k<11;k++)
-                        {
-                            //Console.WriteLine(digitsInt[k] + "and " + digitsInt[k+1]);
-                            if(digitsInt[k+1] == digitsInt[k] + 1 || digitsInt[k+1] == digitsInt[k])
-                            {
-                               result = true;
-                               continue;
-                            }else
-                            {
-                               result = false;
-                               break;
-                            }
-                               
-                            
-                        }
-                        if(result == true)
-                        {
-                            Console.Write(primes[m] + "and" + primes[n] + "and" + primes[n + 1] + "and" + primes[n + 2] + "\n");
-                            Console.Write("The product has 12 digits " + product + "\n");
-                            Console.Write("the result is" + result +  "\n");
-                        }
-                      
-                        continue;
-                        //Console.Write("The product does not have 12 digits " + product + "\n");
-                
-                    }
-                    else
-                    {
-                       
-                        continue;
-                        
-                    }
-
-                }
-
+                return false;
             }
+           
+            String productStr = product.ToString();
+            char[] digitsChar = new char[12];
+            for (int i = 0; i < 12; i++)
+            {
+                digitsChar[i] = Convert.ToChar(productStr[i]);
+            }
+
+            int[] digitsInt = new int[12];
+            for (int i = 0; i < 12; i++)
+            {
+                digitsInt[i] = Convert.ToInt32(new String(digitsChar[i], 1));
+            }
+
+            for (int k = 1; k < 12; k++)
+            {
+                int diff = digitsInt[k] - digitsInt[k - 1];
+                if (diff != 0 && diff != 1)
+                {
+                    return  false;
+                   
+                }
+            }
+
+            return true;
         }
     }
 }
